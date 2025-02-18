efec75e58a5a77d1df464b7399a324a368f9e574
kiprutobrysonn <kiprutobrysonn@gmail.com>
2025-02-18 11:07:20 +0300

    Finished setting up default rails authentication and views for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/signup", to: "users#new"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+
+  get "/login", to: "sessions#new", as: :new_session
+  post "/login", to: "sessions#create", as: :session
+  delete "/logout", to: "sessions#destroy", as: :logout
+
+  root to: "users#new"
+
+  resources :passwords, param: :token
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
